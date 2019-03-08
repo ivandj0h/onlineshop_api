7b714110e6bbb9915a602444a0802e1e13ad5c25
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-08 18:30:36 +0700

    modify routes & activate eloquent function

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,25 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$app->group(['prefix' => 'api'], function() use ($app) {     
+    //product route     
+    $app->get('products', 'ProductController@index');
+    $app->get('products/{id}', 'ProductController@show');
+    $app->post('products', 'ProductController@store');
+    $app->put('products/{id}', 'ProductController@update');
+    $app->delete('products/{id}', 'ProductController@delete');
+    $app->get('categories', 'CategoryController@index');
+    $app->get('categories/{id}', 'CategoryController@show');
+    $app->post('categories', 'CategoryController@store');
+    $app->put('categories/{id}', 'CategoryController@update');
+    $app->delete('categories/{id}', 'CategoryController@delete'); 
 });
+
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
+
+
 
 // How to Add Key on Lumen
 // $router->get('/key', function() {
