e01793a23a11b6699d68954c1d6ca3f09927036a
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-08 18:25:46 +0700

    create category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace AppHttpControllers;
+
+use AppCategory;
+use IlluminateHttpRequest;
+
+class CategoryController extends Controller {
+    
+    public function index(){
+        $categories = Category::all();
+        
+        return response()->json($categories);
+    }
+    
+    public function store(Request $request){
+        Category::create($request->all());
+        
+        return response()->json(['message' => 'Successfull create new category']);
+    }
+    
+    public function show($id){
+        $category = Category::find($id);
+        
+        return response()->json($category);
+    }
+    
+    public function update(Request $request, $id){
+        $category = Category::find($id);
+        $category->update($request->all());
+        
+        return response()->json(['message' => 'Successfull update category']);
+    }
+    
+    public function delete($id){
+        Category::destroy($id);
+        
+        return response()->json(['message' => 'Successfull delete category']);
+    }
+}
