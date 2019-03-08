55391a4b749af1596a41428b89e6d273d5812e28
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-08 18:00:24 +0700

    create migrations

---
+++ database/migrations/2019_03_08_105515_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products',function(Blueprint $table){             
+            $table->increments('id');             
+            $table->string('name');             
+            $table->integer('category_id');             
+            $table->string('slug');             
+            $table->integer('price');             
+            $table->integer('weight');             
+            $table->text('description');             
+            $table->timestamps();         
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
